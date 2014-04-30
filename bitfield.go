e96b3d3150fb01be01f9093fc23442f6901b54dd
matt forbes <matt@freshplum.com>
2014-04-29 18:10:46 -0700

    remove bounds checking, yolo

--- bitfield.go
+++ bitfield.go
@@ -12,29 +12,16 @@ func New(n int) BitField {
 }
 
 func (b BitField) Set(i uint32) {
-	if (int(i) > len(b) * 8) {
-		return
-	}
-
 	idx, offset := (i / 8), (i % 8)
-
 	b[idx] |= (1 << uint(offset))
 }
 
 func (b BitField) Clear(i uint32) {
-	if (int(i) > len(b) * 8) {
-		return
-	}
-
 	idx, offset := (i / 8), (i % 8)
 	b[idx] &= ^(1 << uint(offset))
 }
 
 func (b BitField) Test(i uint32) bool {
-	if (int(i) > len(b) * 8) {
-		return false
-	}
-
 	idx, offset := (i / 8), (i % 8)
 	return (b[idx] & (1 << uint(offset))) != 0
 }
